0f8c1d188c29713460c076b7b86f4b8be27a0927
lc3267 <melvin.strobl@kit.edu>
2022-08-10 17:44:04 +0200

    added transpile to support cirqs qasm import

--- utils.py
+++ utils.py
@@ -3,9 +3,11 @@
 #import qiskit as q
 from qiskit.circuit.random import random_circuit
 from qiskit.circuit import QuantumCircuit
+from qiskit.compiler import transpile
 import cirq
 #import config
 
 def get_random_qasm_circuit(qubits, depth, seed):
     qc = random_circuit(qubits, depth, max_operands=3, measure=True, seed=seed)
+    qc = transpile(qc, basis_gates=['u1', 'u2', 'u3', 'cx', 'id', 'x', 'y', 'z', 'h', 's', 't', 'rx', 'ry', 'rz', 'cx', 'cy', 'cz', 'ch', 'swap', 'ccx', 'cswap'])
     return qc.qasm()
