7043b0915e7e55b4214a3f831f66974ea8c20ef9
lc3267 <melvin.strobl@kit.edu>
2022-09-27 15:50:28 +0200

    reduced to fit tests on ibmq

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 shots_list = [None, *[2**s for s in range(7, 12)]]
 seed = 10000
-evals = 20
-qubits = 10
+evals = 1
+qubits = 7
 depth = 10
 consistent_circuit = True
-real_backend = "ibmq_quito"
+real_backend = "ibm_oslo"
