8618b7c86165b9f109b478cb378c5575dbc22a5f
lc3267 <melvin.strobl@kit.edu>
2023-02-16 11:48:16 +0100

    initial pipeline registry

--- src/quafel/pipeline_registry.py
+++ src/quafel/pipeline_registry.py
@@ -4,6 +4,9 @@ from typing import Dict
 from kedro.framework.project import find_pipelines
 from kedro.pipeline import Pipeline
 
+from quafel.pipelines import data_generation as dg
+from quafel.pipelines import data_science as ds
+
 
 def register_pipelines() -> Dict[str, Pipeline]:
     """Register the project's pipelines.
@@ -11,6 +14,10 @@ def register_pipelines() -> Dict[str, Pipeline]:
     Returns:
         A mapping from pipeline names to ``Pipeline`` objects.
     """
-    pipelines = find_pipelines()
-    pipelines["__default__"] = sum(pipelines.values())
-    return pipelines
+    dg_pipelines = dg.create_pipeline()
+    ds_pipelines = ds.create_pipeline()
+
+    return {
+        "default": dg_pipelines["pl_generate_and_log_circuit"]
+        + ds_pipelines["pl_measure_execution_durations"],
+    }
