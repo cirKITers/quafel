e18d15e60128732ed57279f8b80453e3046ed9b9
lc3267 <melvin.strobl@kit.edu>
2023-02-17 17:24:20 +0100

    implemented eval matrix gen

--- src/quafel/pipelines/data_generation/nodes.py
+++ src/quafel/pipelines/data_generation/nodes.py
@@ -1,14 +1,17 @@
 from qiskit.circuit.random import random_circuit
 from qiskit.compiler import transpile
 
+
 def log_circuit(qasm_circuit):
-    return {
-        "circuit_image": None
-    }
+    return {"circuit_image": None}
 
-def generate_random_qasm_circuit(qubits:int, depth:int, measure:bool, seed:int):
 
-    qc = random_circuit(qubits, depth, max_operands=3, measure=measure, seed=seed)
+def generate_random_qasm_circuit(
+    qubits: int, depth: int, measure: bool, seed: int
+):
+    qc = random_circuit(
+        qubits, depth, max_operands=3, measure=measure, seed=seed
+    )
     qc = transpile(
         qc,
         basis_gates=[
@@ -35,8 +38,24 @@ def generate_random_qasm_circuit(qubits:int, depth:int, measure:bool, seed:int):
         ],
     )
 
+    return {"qasm_circuit": qc.qasm()}
+
+
+def generate_evaluation_matrix_dataset(
+    min_qubits: int,
+    max_qubits: int,
+    qubits_increment: int,
+    min_depth: int,
+    max_depth: int,
+    depth_increment: int,
+    min_shots: int,
+    max_shots: int,
+    shots_increment: int,
+):
+    qubits = range(min_qubits, max_qubits, qubits_increment)
+    depths = range(min_depth, max_depth, depth_increment)
+    shots = range(min_shots, max_shots, shots_increment)
+
     return {
-        "qasm_circuit":qc.qasm()
+        "evaluation_matrix_dataset": [qubits, depths, shots],
     }
-
-
