f8600ef6826747d483265f0c0066188b20a57eb6
lc3267 <melvin.strobl@kit.edu>
2023-02-20 18:08:26 +0100

    added aggregation method

--- src/quafel/pipelines/data_science/nodes.py
+++ src/quafel/pipelines/data_science/nodes.py
@@ -1,28 +1,50 @@
 from . import frameworks as fw
 import time
+import pandas as pd
+
 
 # def execute_circuit(execute_method:callable, n_shots:int, **kwargs):
 #     result = execute_method(shots=n_shots, **kwargs)
 #     return result
 
 
+def aggregate_evaluations(*args):
+    return {
+        "execution_durations": pd.DataFrame(
+            {f"{i}": args[i] for i in range(0, len(args) // 2)}
+        ),
+        "execution_results": pd.DataFrame(
+            {f"{i}": args[i] for i in range(len(args) // 2, len(args))}
+        ),
+    }
 
-def measure_execution_durations(evaluations:int, qasm_circuit:any, framework_identifier:str, n_shots:int, **kwargs):
+
+def measure_execution_durations(
+    evaluations: int,
+    qasm_circuit: any,
+    framework_identifier: str,
+    n_shots: int,
+    **kwargs,
+):
     try:
         framework = getattr(fw, framework_identifier)
     except AttributeError:
-        raise AttributeError(f"Framework identifier does not match one of the existing frameworks. Existing frameworks are {fw}")
+        raise AttributeError(
+            f"Framework identifier does not match one of the existing frameworks. Existing frameworks are {fw}"
+        )
 
-    framework_instance = framework(qasm_circuit=qasm_circuit, n_shots=n_shots, **kwargs)
+    framework_instance = framework(
+        qasm_circuit=qasm_circuit, n_shots=n_shots, **kwargs
+    )
 
     execution_durations = []
     for eval in range(evaluations):
         start = time.time_ns()
         results = framework_instance.execute()
         end = time.time_ns()
-        execution_durations.append(end-start)
+        execution_durations.append(end - start)
 
     return {
-        "execution_durations":execution_durations,
-        "execution_results":results
+        "execution_duration": execution_durations,
+        "execution_result": results,
     }
