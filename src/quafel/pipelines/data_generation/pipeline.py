5dd2f403fc3e410a02d9ae64bf6adc325925c9c3
lc3267 <melvin.strobl@kit.edu>
2023-02-20 18:17:00 +0100

    added parallelizable pipelines

--- src/quafel/pipelines/data_generation/pipeline.py
+++ src/quafel/pipelines/data_generation/pipeline.py
@@ -6,33 +6,81 @@ generated using Kedro 0.18.3
 from kedro.pipeline import Pipeline, node, pipeline
 from quafel.pipelines.data_generation.nodes import (
     log_circuit,
-    generate_random_qasm_circuit
+    generate_random_qasm_circuit,
+    part_generate_random_qasm_circuit,
+    generate_evaluation_matrix,
+    generate_evaluation_partitions,
 )
 
 
-def create_pipeline(**kwargs) -> dict:
+def create_pipeline(n_partitions=1, **kwargs) -> dict:
     nd_log_circuit = node(
         func=log_circuit,
-        inputs={
-            "qasm_circuit":"qasm_circuit"
-        },
-        outputs=
-        {
-            "circuit_image":"circuit_image"
-        }
+        inputs={"qasm_circuit": "qasm_circuit"},
+        outputs={"circuit_image": "circuit_image"},
     )
 
     nd_generate_random_qasm_circuit = node(
         func=generate_random_qasm_circuit,
         inputs={
-            "qubits":"params:qubits",
-            "depth":"params:depth",
-            "measure":"params:measure",
-            "seed":"params:seed",
+            "qubits": "params:qubits",
+            "depth": "params:depth",
+            "seed": "params:seed",
         },
         outputs={
-            "qasm_circuit":"qasm_circuit",
-        }
+            "qasm_circuit": "qasm_circuit",
+        },
+    )
+
+    nd_generate_evaluation_matrix = node(
+        func=generate_evaluation_matrix,
+        inputs={
+            "min_qubits": "params:min_qubits",
+            "max_qubits": "params:max_qubits",
+            "qubits_increment": "params:qubits_increment",
+            "min_depth": "params:min_depth",
+            "max_depth": "params:max_depth",
+            "depth_increment": "params:depth_increment",
+            "min_shots": "params:min_shots",
+            "max_shots": "params:max_shots",
+            "max_shots": "params:max_shots",
+            "shots_increment": "params:shots_increment",
+        },
+        outputs={
+            "evaluation_matrix": "evaluation_matrix",
+        },
+    )
+
+    nd_generate_evaluation_partitions = node(
+        func=generate_evaluation_partitions,
+        inputs={"evaluation_matrix": "evaluation_matrix"},
+        outputs={
+            "evaluation_partitions": "evaluation_partitions",
+        },
+    )
+
+    pl_parallel_generate_and_log_circuit = pipeline(
+        [
+            nd_generate_evaluation_matrix,
+            nd_generate_evaluation_partitions,
+            *[
+                node(
+                    func=part_generate_random_qasm_circuit,
+                    inputs={
+                        "partition": f"evaluation_partition_{i}",
+                        "seed": "params:seed",
+                    },
+                    outputs={
+                        "qasm_circuit": f"qasm_circuit_{i}",
+                    },
+                    name=f"part_generate_random_qasm_circuit_{i}",
+                )
+                # nd_log_circuit
+                for i in range(n_partitions)
+            ],
+        ],
+        outputs=[f"qasm_circuit_{i}" for i in range(n_partitions)],
+        namespace="data_generation",
     )
 
     pl_generate_and_log_circuit = pipeline(
@@ -40,13 +88,18 @@ def create_pipeline(**kwargs) -> dict:
             nd_generate_random_qasm_circuit,
             # nd_log_circuit
         ],
-        outputs={
-            "qasm_circuit":"qasm_circuit"
-        },
-        namespace="data_generation"
+        outputs={"qasm_circuit": "qasm_circuit"},
+        namespace="data_generation",
     )
 
+    pl_generate_evaluation_matrix = pipeline(
+        [nd_generate_evaluation_matrix, nd_generate_evaluation_partitions],
+        outputs={"evaluation_partitions": "evaluation_partitions"},
+        namespace="data_generation",
+    )
 
     return {
         "pl_generate_and_log_circuit": pl_generate_and_log_circuit,
+        "pl_parallel_generate_and_log_circuit": pl_parallel_generate_and_log_circuit,
+        "pl_generate_evaluation_matrix": pl_generate_evaluation_matrix,
     }
--- src/quafel/pipelines/data_science/pipeline.py
+++ src/quafel/pipelines/data_science/pipeline.py
@@ -5,35 +5,77 @@ generated using Kedro 0.18.3
 
 from kedro.pipeline import Pipeline, node, pipeline
 from quafel.pipelines.data_science.nodes import (
-    measure_execution_durations
+    measure_execution_durations,
+    aggregate_evaluations,
 )
 
+import pandas as pd
 
-def create_pipeline(**kwargs) -> dict:
+
+def create_pipeline(n_partitions=1, **kwargs) -> dict:
     nd_measure_execution_durations = node(
         func=measure_execution_durations,
         inputs={
-            "evaluations":"params:evaluations",
-            "qasm_circuit":"qasm_circuit",
-            "framework_identifier":"params:framework_identifier",
-            "n_shots":"params:n_shots",
+            "evaluations": "params:evaluations",
+            "qasm_circuit": "qasm_circuit",
+            "framework_identifier": "params:framework_identifier",
+            "n_shots": "params:n_shots",
         },
         outputs={
-            "execution_durations":"execution_durations",
-            "execution_results":"execution_results",
-        }
+            "execution_duration": "execution_duration",
+            "execution_result": "execution_result",
+        },
+    )
+
+    pl_parallel_measure_execution_durations = pipeline(
+        [
+            *[
+                node(
+                    func=measure_execution_durations,
+                    inputs={
+                        "evaluations": "params:evaluations",
+                        "qasm_circuit": f"qasm_circuit_{i}",
+                        "framework_identifier": "params:framework_identifier",
+                        "n_shots": "params:n_shots",
+                    },
+                    outputs={
+                        "execution_duration": f"execution_duration_{i}",
+                        "execution_result": f"execution_result_{i}",
+                    },
+                    name=f"measure_execution_durations_{i}",
+                )
+                for i in range(n_partitions)
+            ],
+            node(
+                func=aggregate_evaluations,
+                inputs=[
+                    *[f"execution_duration_{i}" for i in range(n_partitions)],
+                    *[f"execution_result_{i}" for i in range(n_partitions)],
+                ],
+                outputs={
+                    "execution_durations": "execution_durations",
+                    "execution_results": "execution_results",
+                },
+                name=f"aggregate_evaluations",
+            ),
+        ],
+        inputs=[f"qasm_circuit_{i}" for i in range(n_partitions)],
+        outputs={
+            "execution_durations": "execution_duration_partitions",
+            "execution_results": "execution_result_partitions",
+        },
+        namespace="data_science",
     )
 
     pl_measure_execution_durations = pipeline(
-        [
-            nd_measure_execution_durations
-        ],
+        [nd_measure_execution_durations],
         inputs={
-            "qasm_circuit":"qasm_circuit",
+            "qasm_circuit": "qasm_circuit",
         },
-        namespace="data_science"
+        namespace="data_science",
     )
 
     return {
         "pl_measure_execution_durations": pl_measure_execution_durations,
+        "pl_parallel_measure_execution_durations": pl_parallel_measure_execution_durations,
     }
