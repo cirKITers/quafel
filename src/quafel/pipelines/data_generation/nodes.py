eca80e526cee6cc0a0eef3f1a93f31d1af6df3a7
lc3267 <melvin.strobl@kit.edu>
2023-02-20 18:09:04 +0100

    added partition based circuit gen

--- src/quafel/pipelines/data_generation/nodes.py
+++ src/quafel/pipelines/data_generation/nodes.py
@@ -1,17 +1,22 @@
 from qiskit.circuit.random import random_circuit
 from qiskit.compiler import transpile
 
+from typing import List, Dict
+import pandas as pd
+
 
 def log_circuit(qasm_circuit):
     return {"circuit_image": None}
 
 
-def generate_random_qasm_circuit(
-    qubits: int, depth: int, measure: bool, seed: int
-):
-    qc = random_circuit(
-        qubits, depth, max_operands=3, measure=measure, seed=seed
-    )
+def part_generate_random_qasm_circuit(partition, seed=100):
+    qubits = partition[partition.columns[0]][0]
+    depth = partition[partition.columns[0]][1]
+    return generate_random_qasm_circuit(qubits, depth, seed)
+
+
+def generate_random_qasm_circuit(qubits: int, depth: int, seed: int):
+    qc = random_circuit(qubits, depth, max_operands=3, measure=True, seed=seed)
     qc = transpile(
         qc,
         basis_gates=[
@@ -41,7 +46,7 @@ def generate_random_qasm_circuit(
     return {"qasm_circuit": qc.qasm()}
 
 
-def generate_evaluation_matrix_dataset(
+def generate_evaluation_matrix(
     min_qubits: int,
     max_qubits: int,
     qubits_increment: int,
@@ -52,10 +57,27 @@ def generate_evaluation_matrix_dataset(
     max_shots: int,
     shots_increment: int,
 ):
-    qubits = range(min_qubits, max_qubits, qubits_increment)
-    depths = range(min_depth, max_depth, depth_increment)
-    shots = range(min_shots, max_shots, shots_increment)
+    qubits = [i for i in range(min_qubits, max_qubits, qubits_increment)]
+    depths = [i for i in range(min_depth, max_depth, depth_increment)]
+    shots = [i for i in range(min_shots, max_shots, shots_increment)]
 
     return {
-        "evaluation_matrix_dataset": [qubits, depths, shots],
+        "evaluation_matrix": {
+            "qubits": qubits,
+            "depths": depths,
+            "shots": shots,
+        }
     }
+
+
+def generate_evaluation_partitions(evaluation_matrix):
+    partitions = {}
+    idx = 0
+    for q in evaluation_matrix["qubits"]:
+        for d in evaluation_matrix["depths"]:
+            for s in evaluation_matrix["shots"]:
+                partitions[f"{idx}"] = {"qubits": q, "depth": d, "shots": s}
+                idx += 1
+
+    pass
+    return {"evaluation_partitions": pd.DataFrame(partitions)}
