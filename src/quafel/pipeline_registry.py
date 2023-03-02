0edcc726a606b8fd64b19c311a57b2b55fe09fca
lc3267 <melvin.strobl@kit.edu>
2023-03-02 17:30:08 +0100

    moved viz pipeline to parallel

--- src/quafel/pipeline_registry.py
+++ src/quafel/pipeline_registry.py
@@ -31,6 +31,7 @@ def register_pipelines() -> Dict[str, Pipeline]:
         + ds_pipelines["pl_measure_execution_durations"],
         "pre": dg_pipelines["pl_generate_evaluation_matrix"],
         "parallel": dg_pipelines["pl_parallel_generate_and_log_circuit"]
-        + ds_pipelines["pl_parallel_measure_execution_durations"],
+        + ds_pipelines["pl_parallel_measure_execution_durations"]
+        + viz_pipelines["pl_visualize_evaluations"],
         "viz": viz_pipelines["pl_visualize_evaluations"],
     }
