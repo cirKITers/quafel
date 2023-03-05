060fadb52574ccf9676c582b3d4ea06846ca3c1a
lc3267 <melvin.strobl@kit.edu>
2023-03-05 11:00:18 +0100

    reading temp files

--- src/quafel/pipeline_registry.py
+++ src/quafel/pipeline_registry.py
@@ -20,7 +20,7 @@ def register_pipelines() -> Dict[str, Pipeline]:
         A mapping from pipeline names to ``Pipeline`` objects.
     """
     partitions = glob.glob("data/02_intermediate/*.csv")
-    figures = [Path(f).stem for f in glob.glob("data/07_reporting/*.json")]
+    figures = [Path(f).stem for f in glob.glob("data/07_reporting/*.tmp")]
 
     dg_pipelines = dg.create_pipeline(n_partitions=len(partitions))
     ds_pipelines = ds.create_pipeline(n_partitions=len(partitions))
