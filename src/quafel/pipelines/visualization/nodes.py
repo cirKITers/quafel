67e1d64463df6cb43e102ff91948fe81ae0e0335
lc3267 <melvin.strobl@kit.edu>
2023-03-08 16:00:59 +0100

    added dynamic time scale

--- src/quafel/pipelines/visualization/nodes.py
+++ src/quafel/pipelines/visualization/nodes.py
@@ -9,6 +9,18 @@ import plotly.express as px
 from typing import Dict
 import pandas as pd
 
+from bisect import bisect_left
+
+
+class design:
+    qual_main = px.colors.qualitative.Set1
+    qual_second = px.colors.qualitative.Pastel1
+
+    seq_main = px.colors.sequential.Plasma
+
+    title_font_size = 20
+    legend_font_size = 18
+
 
 def rgb_to_rgba(rgb_value, alpha):
     """
@@ -18,6 +30,17 @@ def rgb_to_rgba(rgb_value, alpha):
     :return: RGBA Value
     """
     return f"rgba{rgb_value[3:-1]}, {alpha})"
+
+
+def get_lcd_time(pd_time):
+    si_time = [["ns", "us", "ms", "s"], [1, 3, 6, 9], [1, 1e3, 1e6, 1e9]]
+    min_digits = len(str(pd_time.min().min()))
+    max_digits = len(str(pd_time.max().max()))
+
+    mid = min_digits + (max_digits - min_digits) // 2
+    idx = bisect_left(si_time[1], mid) - 1
+
+    return (si_time[0][idx], si_time[2][idx])
 
 
 def extract_framework_name_from_id(identifier):
@@ -49,15 +72,23 @@ def shots_qubits_viz(evaluations_combined: Dict):
                         x=duration_sorted_by_depth["shots"],
                         y=duration_sorted_by_depth["depth"],
                         z=duration_mean,
-                        # colorscale='Viridis'
+                        colorscale=design.seq_main,
                     )
                 ]
             )
             figures[f"framework_{fw}_qubits_{q}"].update_layout(
                 yaxis_title="Circuit Depth",
                 xaxis_title="Num. of Shots",
-                title=f"{framework_name} simulation duration: Circuit Depth and Num. of Shots",
+                title=dict(
+                    text=f"{framework_name} simulation duration: Circuit Depth and Num. of Shots",
+                    font=dict(
+                        size=design.title_font_size,
+                    ),
+                ),
                 hovermode="x",
+                font=dict(
+                    size=design.legend_font_size,
+                ),
             )
 
     return figures
@@ -88,15 +119,23 @@ def shots_depths_viz(evaluations_combined: Dict):
                         x=duration_sorted_by_qubit["shots"],
                         y=duration_sorted_by_qubit["qubits"],
                         z=duration_mean,
-                        # colorscale='Viridis'
+                        colorscale=design.seq_main,
                     )
                 ]
             )
             figures[f"framework_{fw}_depth_{d}"].update_layout(
                 yaxis_title="Num. of Qubits",
                 xaxis_title="Num. of Shots",
-                title=f"{framework_name} simulation duration: Num. of qubits and Num. of Shots",
+                title=dict(
+                    text=f"{framework_name} simulation duration: Num. of qubits and Num. of Shots",
+                    font=dict(
+                        size=design.title_font_size,
+                    ),
+                ),
                 hovermode="x",
+                font=dict(
+                    size=design.legend_font_size,
+                ),
             )
 
     return figures
@@ -106,8 +145,12 @@ def qubits_time_viz(evaluations_combined: Dict):
     figures = {}
 
     # those two color sets are well suited as they correspond regarding their color value but differ from their luminosity and saturation values
-    main_colors_it = iter(px.colors.qualitative.Set2)
-    sec_colors_it = iter(px.colors.qualitative.Pastel2)
+    main_colors_it = iter(design.qual_main)
+    sec_colors_it = iter(design.qual_second)
+
+    si_time, factor_time = get_lcd_time(
+        evaluations_combined.filter(regex=(r"duration_\d."))
+    )
 
     grouped_by_fw = evaluations_combined.groupby("framework")
     for fw, qubit_depth_duration in grouped_by_fw:
@@ -122,23 +165,17 @@ def qubits_time_viz(evaluations_combined: Dict):
 
             for s, fw_qubit_duration in grouped_by_shots:
                 # grouped_by_shots_sorted_by_depth = depth_duration.sort_values('2').groupby('3')
-                duration_sorted_by_qubit = fw_qubit_duration.sort_values(
-                    "qubits"
-                )
+                duration_sorted_by_qubit = fw_qubit_duration.sort_values("qubits")
 
-                durations = duration_sorted_by_qubit.filter(
-                    regex=(r"duration_\d.")
-                )
+                durations = duration_sorted_by_qubit.filter(regex=(r"duration_\d."))
 
-                durations_mean = durations.mean(axis=1)
-                durations_max = durations.max(axis=1)
-                durations_min = durations.min(axis=1)
+                durations_mean = durations.mean(axis=1) / factor_time
+                durations_max = durations.max(axis=1) / factor_time
+                durations_min = durations.min(axis=1) / factor_time
 
                 # image = []
                 # for s, duration in grouped_by_shots_sorted_by_depth:
                 #     image.append(duration['4'].to_numpy())
-
-                traces = []
 
                 if f"shots_{s}_depth_{d}" not in figures:
                     figures[f"shots_{s}_depth_{d}"] = go.Figure()
@@ -177,10 +214,19 @@ def qubits_time_viz(evaluations_combined: Dict):
                     )
                 )
                 figures[f"shots_{s}_depth_{d}"].update_layout(
-                    yaxis_title="Time (ns)",
+                    yaxis_title=f"Time ({si_time})",
                     xaxis_title="Num. of Qubits",
-                    title="Framework simulation duration over num. of qubits",
+                    xaxis=dict(tickmode="linear", tick0=1, dtick=1),
+                    title=dict(
+                        text=f"Framework simulation duration over num. of qubits ({s} shots, circuit depth {d})",
+                        font=dict(
+                            size=design.title_font_size,
+                        ),
+                    ),
                     hovermode="x",
+                    font=dict(
+                        size=design.legend_font_size,
+                    ),
                 )
 
     return figures
