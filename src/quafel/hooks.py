7c9a7d56f879b32b27faae2984e526156bc6af5e
lc3267 <melvin.strobl@kit.edu>
2023-10-20 17:14:48 +0200

    fixed hooks not working properly on changing configuration
    Signed-off-by: lc3267 <melvin.strobl@kit.edu>

--- src/quafel/hooks.py
+++ src/quafel/hooks.py
@@ -45,9 +45,13 @@ class PipelineHooks:
             tempFiles = glob.glob("data/02_intermediate/*.csv")
             for f in tempFiles:
                 os.remove(f)
+            # tempFiles = glob.glob("data/07_reporting/*.tmp")
+            # for f in tempFiles:
+            #     os.remove(f)
 
         if (
-            run_params["pipeline_name"] == "measure"
+            run_params["pipeline_name"] is None  # Running the Default pipeline
+            or run_params["pipeline_name"] == "measure"
             or run_params["pipeline_name"] == "prepare"
         ):
             tempFiles = glob.glob("data/04_execution_results/*.csv")
@@ -66,10 +70,102 @@ class PipelineHooks:
 
         log.info(f"Run took {self.finish_run - self.start_run}s")
 
-        if run_params["pipeline_name"] == "visualize":
-            tempFiles = glob.glob("data/07_reporting/*.tmp")
+        # after visualization, we don't need the tmp files anymore
+        # if (
+        #     run_params["pipeline_name"] is None  # Running the Default pipeline
+        #     or run_params["pipeline_name"] == "visualize"
+        # ):
+
+        if run_params["pipeline_name"] != "prepare":
+            tempFiles = glob.glob("data/02_intermediate/*.csv")
             for f in tempFiles:
                 os.remove(f)
+            tempFiles = glob.glob("data/04_execution_results/*.csv")
+            for f in tempFiles:
+                os.remove(f)
+            tempFiles = glob.glob("data/05_execution_durations/*.csv")
+            for f in tempFiles:
+                os.remove(f)
+
+        # Cleanup all previously generated temp files, we will generate them again below
+        tempFiles = glob.glob("data/07_reporting/*.tmp")
+        for f in tempFiles:
+            os.remove(f)
+
+        if run_params["pipeline_name"] == "prepare":
+            # ----------------------------------------------------------------
+            # This section ensures that the reporting dictionary always contains the proper output data catalogs so that kedro-viz is happy
+            # ----------------------------------------------------------------
+
+            try:
+                # get the evaluation matrix where information about all the possible combinations is stored
+                evaluation_matrix = (
+                    catalog.datasets.data_generation__evaluation_matrix.load()
+                )
+            except Exception as e:
+                log.exception(e)
+                return
+
+            # generate a list of names that will be used as plots later in the visualization pipeline
+            # If you add new visualization outputs, you must also create the file names here
+            names = []
+            for fw in evaluation_matrix["frameworks"]:
+                for q in evaluation_matrix["qubits"]:
+                    names.append(f"{fw}_qubits_{q}")
+                for d in evaluation_matrix["depths"]:
+                    names.append(f"{fw}_depth_{d}")
+                for s in evaluation_matrix["shots"]:
+                    names.append(f"{fw}_shots_{s}")
+
+            for d in evaluation_matrix["depths"]:
+                for s in evaluation_matrix["shots"]:
+                    names.append(f"shots_{s}_depth_{d}")
+
+            for d in evaluation_matrix["depths"]:
+                for q in evaluation_matrix["qubits"]:
+                    names.append(f"qubits_{q}_depth_{d}")
+
+            for s in evaluation_matrix["shots"]:
+                for q in evaluation_matrix["qubits"]:
+                    names.append(f"shots_{s}_qubits_{q}")
+
+            # use the dummy dataset to get the version of the current kedro run, so that it matches the ones from the versioned datasets
+            version = Version(
+                None, catalog.datasets.dummy_versioned_dataset._version.save
+            )
+
+            # iterate all the names and create a JSONDataSet (plotly) for each one
+            for name in names:
+                filepath = os.path.join("data/07_reporting/", f"{name}.json")
+
+                dataset_template = JSONDataSet(filepath=filepath, version=version)
+                catalog.add(name, dataset_template)
+
+                # create a dictionary if necessary (versioned datasets need dictionaries)
+                try:
+                    os.mkdir(filepath)
+                except FileExistsError:
+                    # directory already exists
+                    pass
+
+                # create a .tmp file which we will use later in the pipeline_registry to create node outputs dynamically
+                with open(os.path.join("data/07_reporting/", f"{name}.tmp"), "w") as fw:
+                    fw.write("")
+
+    @hook_impl
+    def on_pipeline_error(self, run_params: Dict[str, Any], pipeline, catalog):
+        tempFiles = glob.glob("data/02_intermediate/*.csv")
+        for f in tempFiles:
+            os.remove(f)
+        tempFiles = glob.glob("data/04_execution_results/*.csv")
+        for f in tempFiles:
+            os.remove(f)
+        tempFiles = glob.glob("data/05_execution_durations/*.csv")
+        for f in tempFiles:
+            os.remove(f)
+        tempFiles = glob.glob("data/07_reporting/*.tmp")
+        for f in tempFiles:
+            os.remove(f)
 
 
 class DataCatalogHooks:
@@ -124,60 +220,3 @@ class DataCatalogHooks:
             execution_result_name = f"execution_result_{Path(execution_result).stem}"
             input_dataset = CSVDataSet(filepath=execution_result)
             catalog.add(execution_result_name, input_dataset)
-
-        # ----------------------------------------------------------------
-        # This section ensures that the reporting dictionary always contains the proper output data catalogs so that kedro-viz is happy
-        # ----------------------------------------------------------------
-
-        try:
-            # get the evaluation matrix where information about all the possible combinations is stored
-            evaluation_matrix = (
-                catalog.datasets.data_generation__evaluation_matrix.load()
-            )
-        except Exception as e:
-            log.exception(e)
-            return
-
-        # generate a list of names that will be used as plots later in the visualization pipeline
-        # If you add new visualization outputs, you must also create the file names here
-        names = []
-        for fw in evaluation_matrix["frameworks"]:
-            for q in evaluation_matrix["qubits"]:
-                names.append(f"{fw}_qubits_{q}")
-            for d in evaluation_matrix["depths"]:
-                names.append(f"{fw}_depth_{d}")
-            for s in evaluation_matrix["shots"]:
-                names.append(f"{fw}_shots_{s}")
-
-        for d in evaluation_matrix["depths"]:
-            for s in evaluation_matrix["shots"]:
-                names.append(f"shots_{s}_depth_{d}")
-
-        for d in evaluation_matrix["depths"]:
-            for q in evaluation_matrix["qubits"]:
-                names.append(f"qubits_{q}_depth_{d}")
-
-        for s in evaluation_matrix["shots"]:
-            for q in evaluation_matrix["qubits"]:
-                names.append(f"shots_{s}_qubits_{q}")
-
-        # use the dummy dataset to get the version of the current kedro run, so that it matches the ones from the versioned datasets
-        version = Version(None, catalog.datasets.dummy_versioned_dataset._version.save)
-
-        # iterate all the names and create a JSONDataSet (plotly) for each one
-        for name in names:
-            filepath = os.path.join("data/07_reporting/", f"{name}.json")
-
-            dataset_template = JSONDataSet(filepath=filepath, version=version)
-            catalog.add(name, dataset_template)
-
-            # create a dictionary if necessary (versioned datasets need dictionaries)
-            try:
-                os.mkdir(filepath)
-            except FileExistsError:
-                # directory already exists
-                pass
-
-            # create a .tmp file which we will use later in the pipeline_registry to create node outputs dynamically
-            with open(os.path.join("data/07_reporting/", f"{name}.tmp"), "w") as fw:
-                fw.write("")
